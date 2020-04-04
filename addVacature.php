3ab9d1a0449fca7e92b469a6a909c70dfc0be4f2
JustinMintjens <JustinMintjens-Byte@users.noreply.github.com>
2020-04-04 16:12:42 +0200

    Update addVacature.php

--- addVacature.php
+++ addVacature.php
@@ -14,7 +14,7 @@ if (!isset($_SESSION["loggedin"])) {
 	<script src="javascript/functions.js"></script>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
-<body>
+<body style="background-color: #D3D3D3;">
 
 	<div id="mySidenav" class="sidenav">
 		<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
@@ -32,14 +32,11 @@ if (!isset($_SESSION["loggedin"])) {
 	</div>
 
 	<div id="bluehead">
-		<h1 style="position: absolute;">Buurtzorg</h1><button data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="position: absolute; right: 0px; top: 60px; "><img style="width: 4%; opacity: 1; position: fixed; right: 15px; top: 5px;" src="images/EN2.png"></button>
+		<h1 style="position: absolute;">Buurtzorg</h1><button data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="right: 0px; top: 60px; "><img style="width: 4%; opacity: 1; position: absolute; right: 15px; top: 5px;" src="images/EN2.png"></button>
 		<div class="dropdown-menu">
-    <a class="dropdown-item" href="#">Action</a>
-    <a class="dropdown-item" href="#">Another action</a>
-    <a class="dropdown-item" href="#">Something else here</a>
-    <div class="dropdown-divider"></div>
-    <a class="dropdown-item" style="color: red; " href="logout.php">Logout</a>
-  </div>
+		    <a class="dropdown-item" href="#NL">Nederlands</a>
+		    <a class="dropdown-item" href="#EN">Engels</a>
+		</div>
 	</div>
 
 <span id="nav" style="font-size:30px;cursor:pointer" onclick="openNav()">&#9776; Navigatie</span>
@@ -47,7 +44,7 @@ if (!isset($_SESSION["loggedin"])) {
 	
 
 	<div class="flex">
-		<form method="post" action="vacatureProces.php" style="margin-top:5%;">
+		<form method="post" action="vacatureProces.php" style="margin-top:2%;">
 			<p><h2>Maak Nieuwe Vacature</h2></p>
 			<input type="hidden" name="id" value="<?php echo $id; ?>">
 			Titel <input required style="width: 30%; margin-bottom: 5px;" type="text" name="titel"><br>
@@ -79,11 +76,11 @@ if (!isset($_SESSION["loggedin"])) {
 
 
 
-		    Locatie <input required style="width: 30%; margin-bottom: 5px;" type="text" name="locatie"><br>
+		    Locatie <input required style="width: 30%; margin-bottom: 5px;" type="text" name="locatie"><br><br>
 		    VMBO
-		  	<input required type="radio" name="opl-niv" value="VMBO"><br>
+		  	<input required type="radio" name="opl-niv" value="VMBO">
 		  	HAVO
-		  	<input required type="radio" name="opl-niv" value="HAVO"><br>
+		  	<input required type="radio" name="opl-niv" value="HAVO">
 		  	VWO
 		  	<input required type="radio" name="opl-niv" value="VWO"><br>
 
@@ -105,8 +102,6 @@ if (!isset($_SESSION["loggedin"])) {
 			
 		</form>
 	</div>
-
-	
 <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
